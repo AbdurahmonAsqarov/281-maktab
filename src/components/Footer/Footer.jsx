a69623d2f39d9fee65750df0d5f9b24c5d4cb008
abdurahmon <abdurahmongit1@gmail.com>
2024-07-01 15:01:07 +0500

    ok

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
   return (
     <>
-        <h1 className='text-center font-[700] text-[50px] bruh mb-[200px]  '>Bizning maktab <span className='max-md:hidden'>!</span></h1>
+        <h1 id='4' className='text-center text-[#776B5D] font-[700] text-[50px] bruh mb-[200px]  '>Bizning maktab <span className='max-md:hidden'>!</span></h1>
         <div className='px-[120px] mb-[300px] max-md:mx-[-120px] max-md:mb-[150px]'>
 
-        <div className='grid grid-cols-2 items-center gap-[100px] max-md:gap-5 max-md:px-[10px]'>
+        <div className='grid grid-cols-2 text-[#776B5D] items-center gap-[100px] max-md:gap-5 max-md:px-[10px]'>
 
             <img src="src/assets/Us school/photo_2024-06-28_16-17-59.jpg" alt="" className='hover:shadow-2xl ease-in duration-500 hover:translate-y-[-30px] h-[323px] w-[584px] rounded-[30px] max-md:h-[30vh]'/>
             <h1 className='text-[50px] font-[700] bruh2 max-md:text-[20px] max-lg:text-[40px] '>Bizning sport zal</h1>
@@ -23,11 +23,11 @@ const Footer = () => {
         <div className='mb-[50px] flex place-content-center '>
             <img src="src/assets/Us school/photo_2024-06-28_16-17-11.jpg" alt="" className='bruh3 w-[70vw] h-[80vh] rounded-[30px] max-md:h-[40vh] max-lg:h-[60vh]'/>
         </div> 
-        <h1 className='text-[30px] font-[600] text-center mb-[200px] max-md:mb-[100px] max-md:text-[25px]'>Yangi O`zbekistonga  yangicha fikrlaydigan,vatanparvar va bilimli yoshlar kerak.</h1>
+        <h1 className='text-[30px] text-[#776B5D] font-[600] text-center mb-[200px] max-md:mb-[100px] max-md:text-[25px]'>Yangi O`zbekistonga  yangicha fikrlaydigan,vatanparvar va bilimli yoshlar kerak.</h1>
 
-        <div className='border-t-[2px] border-[#000] flex justify-between items-center px-[100px] max-md:px-[20px]'>
-            <h1 className='text-[30px] font-[600] my-[25px] bruh max-md:text-[20px] '>281-maktab</h1>
-            <p className='hover:text-[20px] hover:font-[700] max-md:text-[12px]'><a href="1" className='footer hover:animate-pulse'>Bosh sahifaga qaytish</a></p>
+        <div className='border-t-[2px] bg-[#776B5D] border-[#000] flex justify-between items-center px-[100px] max-md:px-[20px]'>
+            <h1 className='text-[30px] text-[#EBE3D5] font-[600] my-[25px] bruh max-md:text-[20px] '>281-maktab</h1>
+            <p className='hover:text-[20px] text-[#EBE3D5] hover:font-[700] max-md:text-[12px]'><a href="1" className='footer hover:animate-pulse'>Bosh sahifaga qaytish</a></p>
         </div>
     </>
   )
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -19,7 +19,7 @@ const TelClick = () => {
 
 const whiteToggle = () => {
   const blackToWhite = document.querySelector('.Dowhite')
-  blackToWhite.classList.add('text-[#fff]')
+  blackToWhite.classList.add('text-[#EBE3D5]')
 }
   return (
     <>
@@ -28,12 +28,12 @@ const whiteToggle = () => {
      <div className='w-full'>
      <div className='bg-img bg-fixed h-[130vh] '>
             <div className='pt-[200px] max-sm:pt-[100px] text-center '>
-         <h1 className='z-10 text-[80px] max-md:text-[40px] text-[#fff] font-[800] backdrop-blur-sm'><span onClick={whiteToggle} className='Dowhite text-[#000]'>281-sonli</span> o'rta umumiy  </h1>
-         <h1 className='text-[#fff] text-[80px] font-[800] max-md:text-[40px] backdrop-blur-sm '>ta'lim maktabi</h1>
+         <h1 className='z-10 text-[80px] max-md:text-[40px] text-[#EBE3D5] font-[800] backdrop-blur-sm'><span onClick={whiteToggle} className='Dowhite text-[#776B5D]'>281-sonli</span> o'rta umumiy  </h1>
+         <h1 className='text-[#EBE3D5] text-[80px] font-[800] max-md:text-[40px] backdrop-blur-sm '>ta'lim maktabi</h1>
 
          <div className="grid grid-cols-2 gap-y-[5px] mt-[50px]  gap-x-10 max-md:gap-3">
            <div className=' place-content-end flex'>
-           <button  className='ad text-[#fff] flex font-[600] bg-[#000] px-[25px] max-md:w-[170px] max-sm:w-[150px] w-[200px] py-[20px] max-sm:px-[15px] rounded-bl-3xl rounded-tr-3xl justify-between border border-[#fff] hover:border-[#000] hover:text-[#000] hover:bg-[#fff] hover:border-[3px]  ease-in duration-300'>
+           <button  className='ad text-[#EBE3D5] flex font-[600] bg-[#776B5D] px-[25px] max-md:w-[170px] max-sm:w-[150px] w-[200px] py-[20px] max-sm:px-[15px] rounded-bl-3xl rounded-tr-3xl justify-between border border-[#EBE3D5]  hover:border-[3px]  ease-in duration-300'>
                 <h1 className='max-md:text-[14px] '>Manzil</h1>
                 <div className=" ">
                   <TiArrowDownOutline className='icon text-2xl'/>
@@ -41,7 +41,7 @@ const whiteToggle = () => {
             </button>
          </div>
           <div className=' text-end'>
-          <button onClick={TelClick} className=' max-sm:w-[150px] text-[#000] hover:text-[#fff] flex bg-[#fff] hover:bg-[#000] font-[600] px-[25px] max-sm:px-[15px] hover:px-[20px]  max-md:w-[190px] w-[200px] py-[20px] rounded-bl-3xl rounded-tr-3xl justify-between border-[3px] border-[#000] hover:border-[#fff]  hover:w-[180px]  ease-in duration-300 hover:origin-top-left hover:rotate-12'>
+          <button onClick={TelClick} className=' max-sm:w-[150px] text-[#776B5D] hover:text-[#EBE3D5] flex bg-[#EBE3D5] hover:bg-[#776B5D] font-[600] px-[25px] max-sm:px-[15px] hover:px-[20px]  max-md:w-[190px] w-[200px] py-[20px] rounded-bl-3xl rounded-tr-3xl justify-between border-[3px] border-[#B0A695] hover:border-[#EBE3D5]  hover:w-[180px]  ease-in duration-300 hover:origin-top-left hover:rotate-12'>
                 <h1 className='max-md:text-[14px] '>Aloqa uchun</h1>
                 <div className=" text-[#000 ] ">
                 {tel ? (
@@ -54,19 +54,19 @@ const whiteToggle = () => {
           </div>
 
                <div className=' text-end'>
-               <button className='max-sm:w-[150px] text-[#000] font-[600] bg-[#fff] px-[4px] max-md:w-[170px] w-[200px] py-[4px] max-sm:px-[15px] rounded-br-3xl rounded-tl-3xl border-[3px] border-[#000] '>
-               <h1 className='max-md:border-none max-md:text-[10px] border-[3px] border-[#000] px-[10px] py-[10px] rounded-br-3xl rounded-tl-3xl hover:animate-pulse'>Toshkent shahar Chilonzor tumani</h1>
+               <button className='max-sm:w-[150px] text-[#776B5D] font-[600] bg-[#EBE3D5] px-[4px] max-md:w-[170px] w-[200px] py-[4px] max-sm:px-[15px] rounded-br-3xl rounded-tl-3xl border-[3px] border-[#776B5D] '>
+               <h1 className='max-md:border-none max-md:text-[10px] border-[3px] border-[#776B5D] px-[10px] py-[10px] rounded-br-3xl rounded-tl-3xl hover:animate-pulse'>Toshkent shahar Chilonzor tumani</h1>
                
                </button>
                </div>
               
               {tel && (
              <div className=' text-start'>
-             <button className='max-sm:w-[150px] text-[#fff] font-[600] bg-[#000] px-[6px] max-md:w-[170px] w-[200px] py-[6px] max-sm:px-[15px] rounded-br-3xl rounded-tl-3xl border-[2px] border-[#fff] '>
-                  <h1 className='max-md:text-[10px] border-[2px] border-[#fff] px-[10px] py-[10px] rounded-br-3xl rounded-tl-3xl max-md:border-none'>+998 (90) 123 45 67</h1>
+             <button className='max-sm:w-[150px] text-[#EBE3D5] font-[600] bg-[#776B5D] px-[6px] max-md:w-[170px] w-[200px] py-[6px] max-sm:px-[15px] rounded-br-3xl rounded-tl-3xl border-[2px] border-[#EBE3D5] '>
+                  <h1 className='max-md:text-[10px] border-[2px] border-[#EBE3D5] px-[10px] py-[10px] rounded-br-3xl rounded-tl-3xl max-md:border-none hover:animate-pulse'>+998 (90) 123 45 67</h1>
                  
               </button>
-             </div>
+             </div> 
             )}
             
             
