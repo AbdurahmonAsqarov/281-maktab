dbe108d78609a7dee593e977c09ff3b706282be7
abdurahmon <abdurahmongit1@gmail.com>
2024-07-02 15:17:54 +0500

    Testing load image

--- src/components/Swiper/Swiper1.jsx
+++ src/components/Swiper/Swiper1.jsx
@@ -31,49 +31,49 @@ const Swiper1 = () => {
       >
         <SwiperSlide className=''>
           <div className="greenLife flex items-center">
-          <img src="src/assets/swiper/Swiper1/photo_2024-04-06_11-45-34 (2).jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
+          <img src="src/assets/swiper/Swiper1/swiper_1.jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
           <h1 className=' text-[24px] font-[600] text-[#EBE3D5]'>281- maktab hovlisiga <span className='text-[#776B5D]'>"Yashil makon"</span> loyihasi asosida  rayhon va gul ko'chatlari ekildi.</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
           <div className='flex items-center greenLife2'>
-            <img src="src/assets/swiper/Swiper1/photo_2024-04-06_11-45-33 (2).jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
+            <img src="src/assets/swiper/Swiper1/swiper_2.jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
             <h1 className='text-[24px] font-[600] text-[#EBE3D5]'>281- maktab hovlisiga <span className='text-[#776B5D]'>"Yashil makon"</span> loyihasi asosida  rayhon va gul ko'chatlari ekildi.</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className='flex items-center greenLife3'>
-            <img src="src/assets/swiper/Swiper1/photo_2024-04-06_11-45-32 (5).jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
+            <img src="src/assets/swiper/Swiper1/swiper_3.jpg" alt="" className='object-cover h-[500px] rounded-[8px]'/>
             <h1 className='text-[24px] font-[600] text-[#EBE3D5]'>281- maktab hovlisiga <span className='text-[#776B5D]'>"Yashil makon"</span> loyihasi asosida  rayhon va gul ko'chatlari ekildi.</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className=' '>
-            <img src="src/assets/swiper/swiper2/photo_2024-04-05_20-05-00 (5).jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
+            <img src="src/assets/swiper/swiper2/swiper_4.jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
             <h1 className='text-[20px] font-[600] text-[#776B5D] max-lg:text-[14px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi umumta'lim maktablariga <span>“TURON” MAKTAB TEATRI”</span> TANLOVIGA START BERILDI</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className=' '>
-            <img src="src/assets/swiper/swiper2/photo_2024-04-05_20-05-00 (3).jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
+            <img src="src/assets/swiper/swiper2/swiper_5.jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
             <h1 className='text-[20px] font-[600] text-[#776B5D] max-lg:text-[14px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi umumta'lim maktablariga <span>“TURON” MAKTAB TEATRI”</span> TANLOVIGA START BERILDI</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className=' '>
-            <img src="src/assets/swiper/swiper2/photo_2024-04-05_20-05-00 (4).jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
+            <img src="src/assets/swiper/swiper2/swiper_6.jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
             <h1 className='text-[20px] font-[600] text-[#776B5D] max-lg:text-[14px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi umumta'lim maktablariga <span>“TURON” MAKTAB TEATRI”</span> TANLOVIGA START BERILDI</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className=' '>
-            <img src="src/assets/swiper/swiper2/photo_2024-04-05_20-05-00 (9).jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
+            <img src="src/assets/swiper/swiper2/swiper_7.jpg" alt="" className='object-cover h-[60vh] w-full h-[500px] rounded-[8px] max-lg:h-[39vh] max-md:h-[55vh]'/>
             <h1 className='text-[20px] font-[600] text-[#776B5D] max-lg:text-[14px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi umumta'lim maktablariga <span>“TURON” MAKTAB TEATRI”</span> TANLOVIGA START BERILDI</h1>
           </div>
         </SwiperSlide>
         <SwiperSlide className=''>
           <div className='eco1 bg-right  place-centent-end'  >
-            <img src="src/assets/swiper/swiper3/photo_2024-04-05_20-06-40 (4).jpg" alt="" className='h-[60vh] max-md:h-[58vh] max-lg:h-[39vh]'/>
+            <img src="src/assets/swiper/swiper3/swiper_8.jpg" alt="" className='h-[60vh] max-md:h-[58vh] max-lg:h-[39vh]'/>
             <div className='grid text-end  justify-items-end max-lg:text-start max-lg:justify-items-start'>
             <h1 className=' w-[800px] text-[20px] text-[#EBE3D5] max-lg:text-[15px] max-lg:w-[400px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi 281-sonli umumta'lim maktabidagi Ekalogiya burchagi</h1>
             </div>
@@ -81,7 +81,7 @@ const Swiper1 = () => {
         </SwiperSlide>
         <SwiperSlide className='place-content-center'>
         <div className='eco2 bg-right  place-centent-end'  >
-            <img src="src/assets/swiper/swiper3/photo_2024-04-05_20-06-40 (3).jpg" alt="" className='h-[60vh] max-md:h-[58vh] max-lg:h-[39vh]'/>
+            <img src="src/assets/swiper/swiper3/swiper_9.jpg" alt="" className='h-[60vh] max-md:h-[58vh] max-lg:h-[39vh]'/>
             <div className='grid text-end  justify-items-end max-lg:justify-items-start max-lg:text-start'>
             <h1 className=' w-[800px] text-[20px] text-[#EBE3D5] max-lg:text-[15px] max-lg:w-[400px]'>Toshkent shahar Chilonzor tumani Maktabgacha va maktab ta'limi bo'limi tasarrufidagi 281-sonli umumta'lim maktabidagi Ekalogiya burchagi</h1>
             </div>
